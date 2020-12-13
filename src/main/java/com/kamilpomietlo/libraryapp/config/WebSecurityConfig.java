efd7831cbd804e8786f46c3130ca5c23a547391c
mortesto <kamilpomietlo1@gmail.com>
2020-12-13 17:07:49 +0100

    implemented basic logging in, closes #18

--- src/main/java/com/kamilpomietlo/libraryapp/config/WebSecurityConfig.java
+++ src/main/java/com/kamilpomietlo/libraryapp/config/WebSecurityConfig.java
@@ -1,52 +1,50 @@
 package com.kamilpomietlo.libraryapp.config;
 
-import com.kamilpomietlo.libraryapp.services.UserService;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.builders.WebSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 
 @Configuration
+@EnableWebSecurity
 public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
-    private final UserService userService;
-
-    public WebSecurityConfig(UserService userService) {
-        this.userService = userService;
-    }
-
-    @Bean
-    public BCryptPasswordEncoder bCryptPasswordEncoder() {
-        return new BCryptPasswordEncoder();
-    }
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         http
                 .authorizeRequests()
-                // permitting everything right now
-                .antMatchers("/**").permitAll()
-                .anyRequest()
-                .authenticated()
-                .and()
+                        // todo permitting everything right now
+                        .antMatchers("/**").permitAll()
+                        .anyRequest().authenticated()
+                        .and()
                 .formLogin()
-                .loginPage("/login").permitAll();
+                        .loginPage("/login")
+                        .and()
+                .logout().permitAll();
     }
 
     @Override
-    public void configure(WebSecurity web) throws Exception {
+    public void configure(WebSecurity web) {
         web
-                .ignoring()
-                .antMatchers("/h2-console/**");
+                .ignoring().antMatchers("/h2-console/**");
     }
 
-    @Autowired
-    public void configureGlobal(AuthenticationManagerBuilder auth) throws Exception {
-        // userService?
-        auth.userDetailsService(userDetailsService())
-                .passwordEncoder(bCryptPasswordEncoder());
+    @Override
+    public void configure(AuthenticationManagerBuilder auth) throws Exception {
+        auth.inMemoryAuthentication()
+                .passwordEncoder(passwordEncoder())
+                .withUser("user@wp.pl")
+                .password(passwordEncoder().encode("pass"))
+                .roles("USER");
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
     }
 }
