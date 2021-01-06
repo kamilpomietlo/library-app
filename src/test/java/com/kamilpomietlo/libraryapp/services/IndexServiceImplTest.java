a58ba69846ed3783b78b6801dc99cbf4401267a6
mortesto <kamilpomietlo1@gmail.com>
2021-01-06 16:03:33 +0100

    added index service tests

---
+++ src/test/java/com/kamilpomietlo/libraryapp/services/IndexServiceImplTest.java
@@ -0,0 +1,144 @@
+package com.kamilpomietlo.libraryapp.services;
+
+import com.kamilpomietlo.libraryapp.model.Author;
+import com.kamilpomietlo.libraryapp.model.Book;
+import com.kamilpomietlo.libraryapp.repositories.AuthorRepository;
+import com.kamilpomietlo.libraryapp.repositories.BookRepository;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+import java.util.HashSet;
+import java.util.Set;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.mockito.ArgumentMatchers.anyString;
+import static org.mockito.Mockito.*;
+
+@ExtendWith(MockitoExtension.class)
+class IndexServiceImplTest {
+
+    private IndexService indexService;
+
+    @Mock
+    BookRepository bookRepository;
+
+    @Mock
+    AuthorRepository authorRepository;
+
+    @BeforeEach
+    void setUp() {
+        indexService = new IndexServiceImpl(bookRepository, authorRepository);
+    }
+
+    @Test
+    void searchByBookAndAuthor() {
+        // given
+        Set<Author> authors = new HashSet<>();
+        Set<Book> books = new HashSet<>();
+
+        Author author = new Author();
+        author.setId(1L);
+        author.setName("name");
+
+        Book book = new Book();
+        book.setId(1L);
+        book.setTitle("title");
+        book.addAuthor(author);
+
+        books.add(book);
+        authors.add(author);
+
+        when(bookRepository.findByTitleIgnoreCaseContaining(anyString())).thenReturn(books);
+        when(authorRepository.findByNameIgnoreCaseContaining(anyString())).thenReturn(authors);
+
+        // when
+        Set<Book> bookSet = indexService.searchByBookAndAuthor(book, author);
+
+        // then
+        assertNotNull(bookSet);
+        assertEquals(1, bookSet.size());
+        verify(bookRepository, times(1)).findByTitleIgnoreCaseContaining(anyString());
+        verify(authorRepository, times(1)).findByNameIgnoreCaseContaining(anyString());
+    }
+
+    @Test
+    void searchByBookAndAuthorEmptyFields() {
+        // given
+        Author author = new Author();
+        author.setId(1L);
+        author.setName("");
+
+        Book book = new Book();
+        book.setId(1L);
+        book.setTitle("");
+
+        // when
+        Set<Book> bookSet = indexService.searchByBookAndAuthor(book, author);
+
+        // then
+        assertNotNull(bookSet);
+        assertEquals(0, bookSet.size());
+        verify(bookRepository, times(0)).findByTitleIgnoreCaseContaining(anyString());
+        verify(authorRepository, times(0)).findByNameIgnoreCaseContaining(anyString());
+    }
+
+    @Test
+    void searchByBookAndAuthorOnlyBook() {
+        // given
+        Set<Book> books = new HashSet<>();
+
+        Author author = new Author();
+        author.setId(1L);
+        author.setName("");
+
+        Book book = new Book();
+        book.setId(1L);
+        book.setTitle("title");
+        book.addAuthor(author);
+
+        books.add(book);
+
+        when(bookRepository.findByTitleIgnoreCaseContaining(anyString())).thenReturn(books);
+
+        // when
+        Set<Book> bookSet = indexService.searchByBookAndAuthor(book, author);
+
+        // then
+        assertNotNull(bookSet);
+        assertEquals(1, bookSet.size());
+        verify(bookRepository, times(1)).findByTitleIgnoreCaseContaining(anyString());
+        verify(authorRepository, times(0)).findByNameIgnoreCaseContaining(anyString());
+    }
+
+    @Test
+    void searchByBookAndAuthorOnlyAuthor() {
+        // given
+        Set<Author> authors = new HashSet<>();
+
+        Author author = new Author();
+        author.setId(1L);
+        author.setName("name");
+
+        Book book = new Book();
+        book.setId(1L);
+        book.setTitle("");
+        book.addAuthor(author);
+
+        authors.add(author);
+
+        when(authorRepository.findByNameIgnoreCaseContaining(anyString())).thenReturn(authors);
+
+        // when
+        Set<Book> bookSet = indexService.searchByBookAndAuthor(book, author);
+
+        // then
+        assertNotNull(bookSet);
+        assertEquals(1, bookSet.size());
+        verify(bookRepository, times(0)).findByTitleIgnoreCaseContaining(anyString());
+        verify(authorRepository, times(1)).findByNameIgnoreCaseContaining(anyString());
+    }
+}
