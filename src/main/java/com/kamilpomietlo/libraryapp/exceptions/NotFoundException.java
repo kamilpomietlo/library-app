9b2ef24e033f4b015a3e4cfc35eaa19a96a878bf
mortesto <kamilpomietlo1@gmail.com>
2021-01-29 12:50:34 +0100

    added comments to custom exception, #50

--- src/main/java/com/kamilpomietlo/libraryapp/exceptions/NotFoundException.java
+++ src/main/java/com/kamilpomietlo/libraryapp/exceptions/NotFoundException.java
@@ -3,6 +3,9 @@ package com.kamilpomietlo.libraryapp.exceptions;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.ResponseStatus;
 
+/**
+ * Is thrown when searched object is not found in repository.
+ */
 @ResponseStatus(HttpStatus.NOT_FOUND)
 public class NotFoundException extends RuntimeException {
 
